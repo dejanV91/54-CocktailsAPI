a7c4fcd9d4288baf51378ebaeb22e42d8e83bee4
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-08 12:17:38 +0100

    fix App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/cocktail/:id" element={<SingleCocktail />} />
         <Route path="*" element={<Error />} />
